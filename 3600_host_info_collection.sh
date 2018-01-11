d70a884a243ed9414fbf05784da8d3e6fe61a453
somedayiamold <luoyou_1988117@163.com>
2018-01-11 15:17:21 +0800

    redirect std output to log file

--- 3600_host_info_collection.sh
+++ 3600_host_info_collection.sh
@@ -7,15 +7,15 @@ readonly UPLOAD_URL="http://192.168.21.142:8000/inventory/uploadMachineInfo"
 #readonly UPLOAD_URL="http://172.16.32.109:8500/inventory/uploadMachineInfo"
 function gather_cpu_info () {
     local physical_cpu_count=$(cat /proc/cpuinfo | grep "physical id" | sort | uniq | wc -l)
-    echo "physical_cpu_count: ${physical_cpu_count}"
+    echo "physical_cpu_count: ${physical_cpu_count}" > machine.log
     local logic_cpu_cores=$(cat /proc/cpuinfo | grep "processor" | wc -l)
-    echo "logic_cpu_cores: ${logic_cpu_cores}"
+    echo "logic_cpu_cores: ${logic_cpu_cores}" >> machine.log
     local cpu_core=$(cat /proc/cpuinfo | grep "cpu cores" | uniq | awk -F : '{print $NF}')
-    echo "cpu_core: ${cpu_core}"
+    echo "cpu_core: ${cpu_core}" >> machine.log
     if ((logic_cpu_cores == physical_cpu_count*cpu_core)); then
-        echo "HT disabled"
+        echo "HT disabled" >> machine.log
     else
-        echo "HT enabled"
+        echo "HT enabled" >> machine.log
     fi
     local cpu_name=$(cat /proc/cpuinfo | grep name | awk -F: '{print $NF}'| uniq)
     if [ $(echo ${cpu_name} | grep -c "GHz") -gt 0 ]; then
@@ -24,12 +24,12 @@ function gather_cpu_info () {
     else
         local trimed_cpu_name=${cpu_name}
     fi
-    echo "cpu_name: ${trimed_cpu_name}"
+    echo "cpu_name: ${trimed_cpu_name}" >> machine.log
     if [ -z ${cpu_frequency} ]; then
         cpu_frequency=$(cat /proc/cpuinfo | grep MHz | uniq | awk -F : '{printf("%.2f\n", $NF/1000)}')
     fi
     cpu_frequency=${cpu_frequency%GHz}
-    echo "cpu_frequency: ${cpu_frequency} GHz"
+    echo "cpu_frequency: ${cpu_frequency} GHz" >> machine.log
     echo '    "physical_cpu_count":' ${physical_cpu_count}',' >> machine_info
     echo '    "cpu_core":' ${cpu_core}',' >> machine_info
     echo '    "logic_cpu_cores":' ${logic_cpu_cores}',' >> machine_info
@@ -39,18 +39,18 @@ function gather_cpu_info () {
 
 function gather_os_info () {
     local product_name=$(dmidecode -s system-product-name)
-    echo "product_name: ${product_name}"
+    echo "product_name: ${product_name}" >> machine.log
     local os_name=$(cat /etc/redhat-release)
-    echo "os_name: ${os_name}"
+    echo "os_name: ${os_name}" >> machine.log
     local kernal=$(uname -r)
-    echo "kernal: ${kernal}"
+    echo "kernal: ${kernal}" >> machine.log
     echo '    "product_name":' '"'${product_name}'",' >> machine_info
     echo '    "os":' '"'${os_name}'",' >> machine_info
     echo '    "kernal":' '"'${kernal}'",' >> machine_info
 }
 
 function gather_nic_info () {
-    type lspci
+    type lspci &> /dev/null
     if [ $? -ne 0 ]; then
         yum install -y pciutils
     fi
@@ -65,17 +65,17 @@ function gather_nic_info () {
     done
     for nic in $(cat /proc/net/dev | grep -Ev ^veth\|^docker\|^flannel\|^team\|Inter\|face\|lo | awk -F : '{print $1}' | sort); do
         local nic_name=$(lspci | grep "^$(ethtool -i ${nic} | awk -F":" '/bus-info/{print $(NF-1)":"$NF}')" | awk -F : '{print $NF}')
-        echo "nic name: ${nic_name}"
+        echo "nic name: ${nic_name}" >> machine.log
         local ip=$(ifconfig ${nic} | grep inet | grep -v inet6 | awk '{print $2}')
         if [ -z "${ip}" ] && [ -n "${nic_list}" ]; then
             local ip=$(echo ${nic_list} | awk '{for(i=1;i<=NF;i++){if(index($i,"'${nic}'")>0){print substr($i,index($i,":")+1,length($i)-1)}}}')
         fi
-        echo "ip: ${ip}"
+        echo "ip: ${ip}" >> machine.log
         local capability=$(ethtool ${nic} | grep -B1 "Supported pause frame use" | grep -v "Supported pause frame use" | awk '{print substr($NF,1,index($NF,"baseT/Full")-1)}')
         if [ -z "${capability}" ]; then
             capability=0
         fi
-        echo "capability: ${capability} Mb/s"
+        echo "capability: ${capability} Mb/s" >> machine.log
         if [ ${counter} -gt 0 ]; then
             echo '        },' >> machine_info
         fi
@@ -101,9 +101,9 @@ function gather_storage_info () {
         else
             local media="HDD"
         fi
-        echo "storage_label: ${storage_label}"
-        echo "volumn: ${volumn} GB"
-        echo "media: ${media}"
+        echo "storage_label: ${storage_label}" >> machine.log
+        echo "volumn: ${volumn} GB" >> machine.log
+        echo "media: ${media}" >> machine.log
         if [ ${counter} -gt 0 ]; then
             echo '        },' >> machine_info
         fi
@@ -125,7 +125,7 @@ function gather_memory_info () {
         else
             local size=$(echo ${line} | awk '{printf("%d", substr($0,1,length($0)-2)/1024)}')
         fi
-        echo "size: ${size} GB"
+        echo "size: ${size} GB" >> machine.log
         if [ ${counter} -gt 0 ]; then
             echo '        },' >> machine_info
         fi
@@ -152,15 +152,15 @@ function upload () {
         local ret_val=$(curl -s -m 180 -w %{http_code} -H "Content-Type: application/json" -X POST -d "$(cat machine_info)" "${UPLOAD_URL}" -o /dev/null)
         echo "upload return code: ${ret_val}"
         if [ ${ret_val} -eq 200 ]; then
-            echo "upload successfully"
+            echo "upload successfully" >> machine.log
             falcon 0 "success"
         else
-            echo "upload failed"
+            echo "upload failed" >> machine.log
             falcon 1 "failed"
             return 1
         fi
     else
-        echo "no update found"
+        echo "no update found" >> machine.log
         falcon 0 "uptodate"
     fi
 }
@@ -170,7 +170,7 @@ function main() {
         mv machine_info machine_info.bak
     fi
     echo '{' > machine_info
-    echo hostname: $(hostname)
+    echo hostname: $(hostname) >> machine.log
     echo '    "hostname":' '"'$(hostname)'",' >> machine_info
     gather_cpu_info
     gather_os_info
