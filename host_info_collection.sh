062a7be50a74f7b86fad4e31a81b35d4ffad1481
罗宥 <luoyou@hualala.com>
2018-01-24 14:28:40 +0800

    fix upload url and nmcli not available problem

--- host_info_collection.sh
+++ host_info_collection.sh
@@ -3,8 +3,8 @@ export LANG="en_US.UTF-8"
 export PATH=$PATH:/usr/sbin/
 current_dir=$(dirname $0)
 cd ${current_dir} || exit 1
-readonly UPLOAD_URL="http://192.168.21.142:8000/inventory/uploadMachineInfo"
-#readonly UPLOAD_URL="http://172.16.32.109:8500/inventory/uploadMachineInfo"
+readonly UPLOAD_URL="http://192.168.21.142:8000/inventory/uploadMachineInfo/"
+#readonly UPLOAD_URL="http://172.16.32.109:8500/inventory/uploadMachineInfo/"
 function gather_cpu_info () {
     local physical_cpu_count=$(cat /proc/cpuinfo | grep "physical id" | sort | uniq | wc -l)
     echo "physical_cpu_count: ${physical_cpu_count}"
@@ -61,11 +61,17 @@ function gather_nic_info () {
     echo '    "nic": [' >> machine_info
     local counter=0
     local nic_list=""
+    #nmcli con show &> /dev/null
+    #local nmcli_available=$?
     for nic in $(cat /proc/net/dev | grep -E ^\(\\s\)*team | awk -F : '{print $1}' | sort); do
         local ip=$(ifconfig ${nic} | grep inet | grep -v inet6 | awk '{print $2}')
-        for i in $(nmcli con show | grep "${nic}-port1" | awk '{print $NF}' | sort); do
-            nic_list="${nic_list} ${i}:${ip}"
-        done
+        #if [ ${nmcli_available} -eq 0 ]; then
+        #    local ethernet=$(nmcli con show | grep "${nic}-port1" | awk '{print $NF}')
+        #    nic_list="${nic_list} ${i}:${ip}"
+        #else
+        local ethernet=$(teamdctl ${nic} state view | grep -B1 'link watches' | grep -v 'link watches' | head -1 | awk '{print $1}')
+        nic_list="${nic_list} ${ethernet}:${ip}"
+        #fi
     done
     for nic in $(cat /proc/net/dev | grep -E ^\(\\s\)*e | awk -F : '{print $1}' | sort); do
         local nic_name=$(lspci | grep "^$(ethtool -i ${nic} | awk -F":" '/bus-info/{print $(NF-1)":"$NF}')" | awk -F : '{print $NF}')
@@ -73,7 +79,7 @@ function gather_nic_info () {
         if [ -z "${nic_name}" ]; then
             continue
         fi
-        local ip=$(ifconfig ${nic} | grep inet | grep -v inet6 | awk '{print $2}')
+        local ip=$(ifconfig ${nic} | grep inet | grep -v inet6 | awk '{if(index($2,"addr:")){print substr($2,index($2,"addr:")+5)}else{print $2}}')
         if [ -z "${ip}" ] && [ -n "${nic_list}" ]; then
             local ip=$(echo ${nic_list} | awk '{for(i=1;i<=NF;i++){if(index($i,"'${nic}'")>0){print substr($i,index($i,":")+1,length($i)-1)}}}')
         fi
